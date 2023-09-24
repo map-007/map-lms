bd07b2ec15bad52a4bd653d9dd07c0714af7cdad
map-007 <min.aungpaing007@gmail.com>
2023-09-25 00:10:24 +0630

    add course des

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseId = async ({ params }: { params: { courseId: string } }) => {
   const userId = auth();
@@ -46,6 +47,10 @@ const CourseId = async ({ params }: { params: { courseId: string } }) => {
             <h2 className="text-xl">Customize your course</h2>            
           </div>
           <TitleForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <DescriptionForm
             initialData={course}
             courseId={course.id}
           />
