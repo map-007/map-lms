d96c5eeaa48751d4f1e2801097cf9e1a60bbac6c
map-007 <min.aungpaing007@gmail.com>
2023-09-24 23:57:29 +0630

    course update

---
+++ lib/db.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client"
+
+declare global {
+    var prima: PrismaClient | undefined
+}
+
+export const db = globalThis.prima || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prima = db;
